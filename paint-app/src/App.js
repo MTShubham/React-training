6e7c76e47d7c3ff290be03e4ee92d2682aa58064
Shubham Dusane <shubham.dusane@marutitech.com>
2023-08-06 23:51:27 +0530

    paint demo using hooks

---
+++ paint-app/src/App.js
@@ -0,0 +1,13 @@
+import Playground from "./Playground";
+import Paint from "./app/components/Paint";
+
+function App() {
+  return (
+    <div className="App">
+      <Playground />
+      <Paint />
+    </div>
+  );
+}
+
+export default App;
